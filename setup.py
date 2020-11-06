4b7880f2cd18d7c9f160650da2eb5f61ffb5f89d
Sasan Rasti <sasanrasti@Sasans-MBP>
2020-11-06 14:53:04 +0100

    fix

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ prod = requirements["tool"]["poetry"]["dependencies"]
 dev = requirements["tool"]["poetry"]["dev-dependencies"]
 
 setup(
-    install_requires=[x + prod[x] if prod[x] != "*" else x for x in prod],
-    extras_require={"dev": [x + dev[x] if dev[x] != "*" else x for x in dev]},
+    install_requires=[x + prod[x].replace("^", "==") if prod[x] != "*" else x for x in prod],
+    extras_require={"dev": [x + dev[x].replace("^", "==") if dev[x] != "*" else x for x in dev]},
 )
