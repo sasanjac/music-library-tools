06418451c7046aecc3df903d80c52e23cda7fd7f
Sasan Rasti <sasanrasti@Sasans-MBP>
2020-11-06 16:21:18 +0100

    fix

--- setup.py
+++ setup.py
@@ -13,6 +13,14 @@ install_requires = [x + prod[x].replace("^", "==") if prod[x] != "*" else x for 
 extras_require = {"dev": [x + dev[x].replace("^", "==") if dev[x] != "*" else x for x in dev]}
 
 setup(
+    name=requirements["tool"]["poetry"]["name"],
+    version=requirements["tool"]["poetry"]["version"],    
+    description=requirements["tool"]["poetry"]["description"],
+    url=requirements["tool"]["poetry"].get("url", ""),
+    author=requirements["tool"]["poetry"]["authors"][0].split(" <")[0],
+    author_email=requirements["tool"]["poetry"]["authors"][0].split(" <")[0].replace(">", ""),
+    license=requirements["tool"]["poetry"].get("license", "None"),
+    packages=[requirements["tool"]["poetry"]["name"].replace("-", "_")],
     install_requires=install_requires,
     extras_require=extras_require,
 )
