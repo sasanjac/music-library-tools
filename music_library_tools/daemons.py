4ac24f031281d60164a627f13334a39f67e19864
Sasan Rasti <sasanrasti@Sasans-MBP>
2020-11-06 16:43:39 +0100

    fix

--- music_library_tools/daemons.py
+++ music_library_tools/daemons.py
@@ -8,22 +8,22 @@ import schedule
 from music_library_tools.music_import_daemon import MusicImportDaemon
 
 
-def run():
-    import_path = Path("/data/import")
-    todo_path = Path("/data/todo")
-    export_electro_path = Path("/data/export_electro")
-    export_general_path = Path("/data/export_general")
+import_path = Path("/data/import")
+todo_path = Path("/data/todo")
+export_electro_path = Path("/data/export_electro")
+export_general_path = Path("/data/export_general")
 
-    mid = MusicImportDaemon(
-        import_path=import_path,
-        todo_path=todo_path,
-        export_electro_path=export_electro_path,
-        export_general_path=export_general_path,
-    )
 
-    mid.import_music()
-    schedule.every(15).minutes.do(mid.import_music)
+mid = MusicImportDaemon(
+    import_path=import_path,
+    todo_path=todo_path,
+    export_electro_path=export_electro_path,
+    export_general_path=export_general_path,
+)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+mid.import_music()
+schedule.every(15).minutes.do(mid.import_music)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
