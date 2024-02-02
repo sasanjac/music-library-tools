15621205055ade73276994cc0500e1d89c1e4270
Sasan Jacob Rasti <sasan.rasti@icloud.com>
2024-02-02 23:20:37 +0100

    back to schedule based daemon

--- music_library_tools/daemons.py
+++ music_library_tools/daemons.py
@@ -1,17 +1,13 @@
 #! /usr/bin/python
 from __future__ import annotations
 
-import os
 import pathlib
 import sys
 import time
 
 import schedule
-import watchdog.events as we
-import watchdog.observers as wo
 from loguru import logger
 
-from music_library_tools import utils
 from music_library_tools.music_cleanup_daemon import MusicCleanupDaemon
 from music_library_tools.music_import_daemon import MusicImportDaemon
 
@@ -22,45 +18,6 @@ import_path = pathlib.Path("/data/import")
 todo_path = pathlib.Path("/data/todo")
 export_electro_path = pathlib.Path("/data/export_electro")
 export_general_path = pathlib.Path("/data/export_general")
-plex_url = os.environ["PLEX_URL"]
-token = os.environ["PLEX_TOKEN"]
-base_path = "/data/export_electro"
-
-
-class MIDHandler(we.FileSystemEventHandler):
-    def __init__(self, mid: MusicImportDaemon) -> None:
-        self._mid = mid
-
-    def on_created(self, event: we.DirCreatedEvent | we.FileCreatedEvent) -> None:
-        src_path = pathlib.Path(event.src_path)
-        if event.is_directory and src_path.exists():
-            for _ in range(20):
-                time.sleep(1)
-                files = [f for f in src_path.iterdir() if f.suffix == ".flac"]
-                if len(files) > 0:
-                    for _ in range(12):
-                        n_files = max(int(f.name.split(" - ")[0]) for f in files)
-                        if n_files == len(files):
-                            self._mid.import_album(album_path=src_path)
-                            if len(list(src_path.parent.iterdir())) == 0:
-                                utils.safe_delete_path(src_path.parent)
-                            return
-
-                        time.sleep(5)
-                        files = [f for f in src_path.iterdir() if f.suffix == ".flac"]
-
-                    logger.error(f"{src_path!s} can not be imported. Missing files.")
-
-
-class MCDHandler(we.FileSystemEventHandler):
-    def __init__(self, mcd: MusicCleanupDaemon) -> None:
-        self._mcd = mcd
-
-    def on_modified(self, event: we.FileModifiedEvent | we.DirModifiedEvent) -> None:
-        src_path = pathlib.Path(event.src_path)
-        if not event.is_directory and src_path.parent.exists():
-            time.sleep(60)
-            self._mcd.cleanup_album(album_path=src_path.parent)
 
 
 mid = MusicImportDaemon(
@@ -71,10 +28,7 @@ mid = MusicImportDaemon(
 )
 
 mid.import_music()
-mid_observer = wo.Observer()
-mid_handler = MIDHandler(mid=mid)
-mid_observer.schedule(mid_handler, import_path, recursive=True)
-mid_observer.start()
+schedule.every(5).minutes.do(mid.import_music)
 
 mcd = MusicCleanupDaemon(
     todo_path=todo_path,
@@ -82,20 +36,8 @@ mcd = MusicCleanupDaemon(
 )
 
 mcd.cleanup_music()
-mcd_observer = wo.Observer()
-mcd_handler = MCDHandler(mcd=mcd)
-mcd_observer.schedule(mcd_handler, todo_path, recursive=True)
-mcd_observer.start()
+schedule.every(5).minutes.do(mcd.cleanup_music)
 
-
-try:
-    while True:
-        schedule.run_pending()
-        time.sleep(10)
-
-except KeyboardInterrupt:
-    mid_observer.stop()
-    mcd_observer.stop()
-
-mid_observer.join()
-mcd_observer.join()
+while True:
+    schedule.run_pending()
+    time.sleep(10)
