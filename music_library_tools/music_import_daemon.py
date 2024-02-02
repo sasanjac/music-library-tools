65e4ae7d533183afc562cdfd1299dfcf2fa18dd8
Sasan Jacob Rasti <sasan.rasti@icloud.com>
2024-02-02 23:27:57 +0100

    increase score

--- music_library_tools/music_import_daemon.py
+++ music_library_tools/music_import_daemon.py
@@ -28,7 +28,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
 }
 
-MIN_SCORE = 800
+MIN_SCORE = 1000
 
 
 @dataclass
